8b61660710c6ba740197dc6ebd1465e24f7667ef
tony-reddington <tony.reddington@gmail.com>
2021-11-02 16:20:26 +0000

    Registering the product,category and subcategory models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product, Category, Subcategory
 
-# Register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Subcategory)

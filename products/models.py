82513150044dc40131ab71b2829115932beb0788
tony-reddington <tony.reddington@gmail.com>
2021-11-03 00:08:24 +0000

    Upadting models to resolve reading errors in django admin

--- products/models.py
+++ products/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=False, blank=False)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -14,7 +14,8 @@ class Category(models.Model):
 
 class Subcategory(models.Model):
     name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=False, blank=False)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.name
@@ -26,11 +27,11 @@ class Subcategory(models.Model):
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     subcategory = models.ForeignKey('Subcategory', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=False, blank=False)
+    sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254, null=False, blank=False)
     details = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(null=False, blank=False)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name

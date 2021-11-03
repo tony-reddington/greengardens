96cec2187c993579a0772ee9fdb2eb2ee7b26c09
tony-reddington <tony.reddington@gmail.com>
2021-11-03 14:11:55 +0000

    Changing subcategory name to sub and migrating

--- products/models.py
+++ products/models.py
@@ -26,7 +26,7 @@ class Subcategory(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    subcategory = models.ForeignKey('Subcategory', null=True, blank=True, on_delete=models.SET_NULL)
+    sub = models.ForeignKey('Subcategory', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254, null=False, blank=False)
     details = models.TextField()

c8b5926857e537f626ba4e917a5e7b4acbc75e83
tony-reddington <tony.reddington@gmail.com>
2021-11-04 12:30:17 +0000

    Customising admin display view for products, categories and subcategories

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,37 @@
 from django.contrib import admin
 from .models import Product, Category, Subcategory
 
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Subcategory)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'sub',
+        'price',
+        'image'
+    )
+
+    ordering = ('sku','sub',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+    ordering = ('name',)
+
+class SubcategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+        'category'
+    )
+
+    ordering = ('friendly_name',)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Subcategory, SubcategoryAdmin)

7ed60c7e5cc43cf9f2402d989f49aa48a4fc58a4
tony-reddington <tony.reddington@gmail.com>
2021-11-04 12:26:49 +0000

    Customising names for categories and subcategories es

--- products/models.py
+++ products/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -13,6 +17,10 @@ class Category(models.Model):
 
 
 class Subcategory(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Subcategories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)

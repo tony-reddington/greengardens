b0c1af468533e682a12d247d454a928521cd1792
tony-reddington <tony.reddington@gmail.com>
2021-11-12 19:53:00 +0000

    Adding OrderLineItem class to ghaccess each product order with quantity and total value

--- checkout/models.py
+++ checkout/models.py
@@ -1,4 +1,10 @@
+import uuid
+
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from products.models import Product
 
 
 class Order(models.Model):
@@ -16,3 +22,10 @@ class Order(models.Model):
     total_order = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     delivery_cost = models.DecimalField(max_digits=5, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank-False, default=0)
+    line_item_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)

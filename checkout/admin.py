fbfa0bddceb991698084703b3bd750ef4d161493
tony-reddington <tony.reddington@gmail.com>
2021-11-13 12:54:34 +0000

    Adding OrderLineItemAdminInline class to update the quantity and adding to OrderAdmin class to make accessible

--- checkout/admin.py
+++ checkout/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 from .models import Order, OrderLineItem
 
 
+class OrderLineItemAdminInline(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('line_item_total',)
+
 class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdminInline,)
+    
     readonly_fields = ('order_number', 'date_of_order',
                        'delivery_cost', 'total_order',
                        'grand_total',)

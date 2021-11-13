b3d356b52fec950647ddce72c882235cfe78c035
tony-reddington <tony.reddington@gmail.com>
2021-11-13 17:16:12 +0000

    Adding imports and basic checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
-# Create your views here.
+import sweetify
+
+from .forms import OrderForm
+
+
+def checkout(request):
+    bag = request.session.get('bag', {})
+    if not bag:
+        sweetify.error(request, 'Ooops',
+                       text="There's no items in your basket", icon='error')
+        return redirect(reverse('home'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form
+    }
+
+    return redirect(request, template, context)

20f12b023b82572c2b5a8d7fe1cdddb44c495485
tony-reddington <tony.reddington@gmail.com>
2021-11-13 12:44:35 +0000

    Adding OrderAdmin class to control ordr fields and display on the admin interface

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ('order_number', 'date_of_order',
+                       'delivery_cost', 'total_order',
+                       'grand_total',)
+
+    list_display = ('order_number', 'date_of_order', 'full_name',
+                    'total_order', 'delivery_cost', 'grand_total',)
+
+    ordering = ('-date_of_order',)

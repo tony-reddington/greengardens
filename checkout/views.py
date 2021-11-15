6eb5133518587501afefbb9aa51886b69ef12207
tony-reddington <tony.reddington@gmail.com>
2021-11-15 10:51:40 +0000

    Adding stripe public and secret key references and the stripe payment intent to the checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,22 +1,40 @@
 from django.shortcuts import render, redirect, reverse
+from django.conf import settings
 
 import sweetify
 
 from .forms import OrderForm
+from bag.contexts import bag_products
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         sweetify.error(request, 'Ooops',
                        text="There's no items in your basket", icon='error')
         return redirect(reverse('home'))
 
+    current_bag = bag_products(request)
+    total = current_bag['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51Jk5DjEGAJ0QdMrrRPD8VlyYaYQXjxFkAzbCOIvuCYmlCxXAx1CNbbIBj6wWD7saHJUfCLZ8DRGkTnvagSy2Zsne00xfQ5wEMu',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)

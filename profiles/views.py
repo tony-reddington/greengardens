cb1e237868b98319da33a6486bc5b9cbd934d4a3
tony-reddington <tony.reddington@gmail.com>
2021-11-16 17:37:00 +0000

    Requesting updated form and  isplaying a  message and getting user orders from profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import ProfileForm
+
+import sweetify
 
 def profile(request):
     """ View to display the users profile """
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            sweetify.success(request, 'Updated',
+                     text="Your address has been updated",
+                     icon='success')
+
+    form = ProfileForm(instance=profile)
+    user_orders = profile.orders.all()
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'user_orders': user_orders,
     }
     return render(request, template, context)

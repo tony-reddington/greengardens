e27adf919a54290d2c80172a487c3cc7a0b1dd80
tony-reddington <tony.reddington@gmail.com>
2021-11-16 20:26:16 +0000

    Changing order of the fields in the profile view so more visually appealing

--- profiles/models.py
+++ profiles/models.py
@@ -9,13 +9,13 @@ from django_countries.fields import CountryField
 class UserProfile(models.Model):
     """ Maintains delivery information and order history """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    default_telephone_number = models.CharField(max_length=20, null=True, blank=True)
     default_address_line1 = models.CharField(max_length=100, null=True, blank=True)
     default_address_line2 = models.CharField(max_length=100, null=True, blank=True)
     default_city_or_town = models.CharField(max_length=50, null=True, blank=True)
     default_county_or_state = models.CharField(max_length=50, null=True, blank=True)
     default_postcode_or_zip = models.CharField(max_length=15, null=True, blank=True)
     default_country = CountryField(blank_label='Country', null=True, blank=True)
+    default_telephone_number = models.CharField(max_length=20, null=True, blank=True)
 
     def __str__(self):
         return self.user.username

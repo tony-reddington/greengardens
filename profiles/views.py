294097e37c48319ec536038c1d9d2a7139c0a898
tony-reddington <tony.reddington@gmail.com>
2021-11-16 16:41:41 +0000

    Adding profile to context to access on the profile.html page

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from .models import UserProfile
 
 def profile(request):
-    """ View to display the users profiles """
+    """ View to display the users profile """
+    profile = get_object_or_404(UserProfile, user=request.user)
     template = 'profiles/profile.html'
     context = {
-
+        'profile': profile,
     }
     return render(request, template, context)

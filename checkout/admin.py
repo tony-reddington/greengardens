b1ebbf47a314afa70cf8d974476c9e55e0852087
tony-reddington <tony.reddington@gmail.com>
2021-11-16 20:27:29 +0000

    Adding list of fields including user_profile to ensure user address is stored

--- checkout/admin.py
+++ checkout/admin.py
@@ -8,10 +8,15 @@ class OrderLineItemAdminInline(admin.TabularInline):
 
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
-    
+
     readonly_fields = ('order_number', 'date_of_order',
                        'delivery_cost', 'total_order',
                        'grand_total',)
+
+    fields = ('order_number', 'user_profile', 'full_name',
+              'email_address', 'telephone_number', 'address_line1',
+              'address_line2', 'city_or_town', 'county_or_state',
+              'postcode_or_zip', 'country')
 
     list_display = ('order_number', 'date_of_order', 'full_name',
                     'total_order', 'delivery_cost', 'grand_total',)

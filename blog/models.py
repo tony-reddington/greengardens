ebad8f8384a35c905e50fc909d7463b80669282a
tony-reddington <tony.reddington@gmail.com>
2021-11-17 14:04:23 +0000

    Adding model for each blog post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    """ Holds Blog posts """
+    title = models.CharField(max_length=100, null=True, blank=True)
+    body = models.TextField()
+    author = models.CharField(max_length=100, null=True, blank=True)
+    publish_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-publish_date']

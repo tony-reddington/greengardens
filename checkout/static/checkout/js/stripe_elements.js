b92a83e267839dcb80251529cfb30aaaef41e38f
tony-reddington <tony.reddington@gmail.com>
2021-11-17 23:00:27 +0000

    Adding missing semi colon

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -68,7 +68,7 @@ form.addEventListener('submit', function (ev) {
             $('#submit-button').attr('disabled', false);
         } else {
             if (result.paymentIntent.status === 'succeeded') {
-                form.submit()
+                form.submit();
             }
         }
     });

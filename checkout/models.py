16f5abb1968ea5e52f08141558b580b77ef25af0
tony-reddington <tony.reddington@gmail.com>
2021-11-18 14:29:32 +0000

    Removing blank space

--- checkout/models.py
+++ checkout/models.py
@@ -12,7 +12,7 @@ from profiles.models import UserProfile
 
 class Order(models.Model):
     order_number = models.CharField(max_length=50, null=False, editable=False)
-    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, 
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
                                      null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email_address = models.EmailField(max_length=250, null=False, blank=False)
